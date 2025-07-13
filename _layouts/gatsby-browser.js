6a407480bbb3b23b7ddea0ec591cb13a59d2e927
pionj <jehakim22oct@naver.com>
2025-07-14 05:13:09 +0900

    test katex plugin

--- _layouts/gatsby-browser.js
+++ _layouts/gatsby-browser.js
@@ -1,1 +1,2 @@
 import './styles/style.css'
+import "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/katex.min.css"
--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -21,12 +21,26 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+    resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          `gatsby-remark-mathjax`,
+          {
+            resolve: `gatsby-remark-katex`,
+            options: {
+              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+              strict: `ignore`
+            }
+          }
         ],
-      },
-    }
+      }
+    },
+    // {
+    //   resolve: `gatsby-transformer-remark`,
+    //   options: {
+    //     plugins: [
+    //       `gatsby-remark-mathjax`,
+    //     ],
+    //   },
+    // }
   ],
 };
