39468a16ad9afd966afdde18ec63281eb90031be
pionj <jehakim22oct@naver.com>
2025-07-14 06:14:11 +0900

    add rehype katex and remark math for experiment

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -23,15 +23,17 @@ module.exports = {
     {
     resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-katex`,
-            options: {
-              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-              strict: `ignore`
-            }
-          }
-        ],
+        remarkPlugins: [ require('remark-math')],
+        rehypePlugins: [require('rehype-katex')],
+        // plugins: [
+        //   {
+        //     resolve: `gatsby-remark-katex`,
+        //     options: {
+        //       // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+        //       strict: `ignore`
+        //     }
+        //   }
+        // ],
       }
     },
     // {
